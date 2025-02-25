1298f3280c539b4049ef7b0ab1e73a169dc80ed7
tayl0rm <mattytaylor2397@gmail.com>
2025-02-25 21:52:19 +0000

    Update call to clamav scanner service

--- odyscan/scanner/scan.go
+++ odyscan/scanner/scan.go
@@ -13,7 +13,7 @@ import (
 // ScanWithClamAV scans extracted files using ClamAV running in another namespace
 func ScanWithClamAV(cfg *config.Config) error {
 	// Construct ClamAV service address using namespace
-	clamavService := fmt.Sprintf("tcp://clamd.%s.svc.cluster.local:%s", cfg.ClamdNamespace, cfg.ClamdPort)
+	clamavService := fmt.Sprintf("tcp://clamav-scanner.%s.svc.cluster.local:%s", cfg.ClamdNamespace, cfg.ClamdPort)
 	clam := clamd.NewClamd(clamavService)
 
 	fmt.Printf("🔍 Connecting to ClamAV at %s\n", clamavService)
